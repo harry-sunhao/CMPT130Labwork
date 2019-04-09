509b208cc009c00d46e2c21524cebbe82fdba018
Harry <36096251+harry-sunhao@users.noreply.github.com>
2019-04-08 17:02:32 -0700

    Create Main.cpp

---
+++ LastFInal/Main.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int row =3,cow=3;
+    int **A=new int* [row];
+    for(int i=0;i<row;i++)
+    {
+        A[i]=new int [cow];
+    }
+    int num=1;
+    for(int i=0;i<cow;i++)
+    {
+        for(int j=0;j<row;j++)
+        {
+            A[j][i]=num;
+            num+=2;
+        }
+    }
+    for(int i=0;i<cow;i++)
+    {
+        for(int j=0;j<row;j++)
+        {
+            cout<<A[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<row;i++)
+    {
+        delete [] A[i];
+    }
+    delete []A;
+    return 0;
+}
